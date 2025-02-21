aa3781868a462c1dabfa016171b3a607c34aff3e
JMi-B <j.zankl@gmx.de>
2025-02-21 12:23:37 +0100

    IfElse abgeschlossen

---
+++ src/VerzweigungSwitch.java
@@ -0,0 +1,18 @@
+
+public class VerzweigungSwitch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Kontrollstrukturen steuern den Programmablaufplan
+		
+		// swich Fallunterscheidung
+		//case = der Fall
+		
+		//(Bedinung) als BEdinung gild nur eine int, char und ein String
+		
+		
+
+	}
+
+}
