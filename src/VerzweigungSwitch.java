33d264372a6e578aa21ddd7813a18b08fe45f95a
Juliane Bonenkamp <JBON182@more2learn.de>
2025-03-04 08:40:55 +0100

    es geht weiter mit switch

---
+++ src/VerzweigungSwitch.java
@@ -0,0 +1,28 @@
+
+public class VerzweigungSwitch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Kontrollstruktruren steuern den Programmablaufplan
+		
+		//switch = Fallunterscheidung
+		//cade = Der Fall
+		
+		// (Bedinung) als Bedinung gilt nur ein int, char und ein String
+		
+		switch (5) {
+		case 0:
+			System.out.println("Die Zahl ist eine 0");
+			break;
+		case 3:
+			System.out.println("Die Zahl ist eine 3");
+			break;
+		case 5:
+			System.out.println("Die Zahl ist eine 5");
+			break;
+		}
+
+	}
+
+}
