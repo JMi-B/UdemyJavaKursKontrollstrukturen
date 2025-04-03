967c7366f0824e273925ee99bdeb15b7de12b2d8
Juliane Bonenkamp <JBON182@more2learn.de>
2025-04-03 12:25:55 +0200

    Schleifen Teil I

---
+++ src/SchleifeFor.java
@@ -0,0 +1,31 @@
+
+public class SchleifeFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// for = sei ...
+
+		/*
+		 * Allgemeiner Aufbau einer for-Schleife
+		 * 
+		 * for(Anfangswert; Abbruchbedinung; Zähler){
+		 * 
+		 * }
+		 */
+
+		for (int i = 0; i < 10; i++) { //++ Operator
+
+			System.out.println(i);
+		}
+		
+		for (int j = 1; j <=10; j ++) {
+			if (j == 5) {
+				System.out.println("Hey, du hast mich gefunden " + j);
+				
+				
+			}
+		}
+	}
+
+}
---
+++ src/SchleifeWhile.java
@@ -0,0 +1,39 @@
+
+public class SchleifeWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//while = solange
+		
+		/* Allgemeiner Aufbau einer while-Schleife
+		 * 
+		 * while(Bedinung){
+		 * 
+		 * Anweisungsblock
+		 * 
+		 * }
+		 * 
+		 * Merke: Die Bedinung kann entweder true 0 wahr oder false = nichtwahr sein
+		 * 
+		 */
+		
+		int count = 1; //Zählervariable
+//		while (count != 6) {
+//			System.out.println("Ich bin " + count);
+//			count ++ ; // hochzählen
+//			
+//		}
+		
+		while(true) {
+			System.out.println("Hi");
+			if (count == 3) {
+				break;
+			}
+			count++; //Zähler
+		}
+		
+
+	}
+
+}
