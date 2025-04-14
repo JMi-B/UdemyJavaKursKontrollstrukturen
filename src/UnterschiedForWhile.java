bcbbd8d919ea830a638148d5fcc8fe370b1a7928
JMi-B <j.zankl@gmx.de>
2025-04-14 18:16:44 +0200

    Kontrollstrukturen abgeschlosssen

--- src/UnterschiedForWhile.java
+++ src/UnterschiedForWhile.java
@@ -12,23 +12,29 @@ public class UnterschiedForWhile {
 //			
 //		}
 
-		for(int i = 1; i<=5; i++) {
-			System.out.println("Astroiere wurde erstellt Nr: " + i);
-		}
+//		for(int i = 1; i<=5; i++) {
+//			System.out.println("Astroiere wurde erstellt Nr: " + i);
+//		}
+//		 Anzahl der Durchläufe bekannt		
+		
+		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Drücke 1 für Schwierigkeitsgrad Leicht \n "
-				+ "Drücke 2 für Schwierigkeitsgrad mittel");
-		
-		
+		System.out.println("Drücke 1 für Schwierigkeitsgrad Leicht \n " + "Drücke 2 für Schwierigkeitsgrad mittel");
+
 		int userEingabe = scan.nextInt();
 		int anzahlGegner = 0;
-		
+
 		if (userEingabe == 1) {
 			anzahlGegner = 10;
-		}else if(userEingabe == 2)
-				while (userEingabe !=10) {
-					
-				}
+		} else if (userEingabe == 2) {
+			anzahlGegner = 15;
+		}
+		while (anzahlGegner != 0) {
+			System.out.println("Gegner erstellt!");
+			anzahlGegner--; //um einen runterzählen
+
+		}
+		scan.close();
 	}
 
 }
